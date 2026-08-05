e6813a749b39492bd2569c45fd74aab122a37253
Your Name <you@example.com>
2026-08-05 16:38:33 -0400

    push

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+chown -R mysql:mysql /var/lib/mysql
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
     
     mysqld_safe &
-    sleep 5
 
-    mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
-    mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DB}\`.* TO \`${MYSQL_USER}\`@'%';"
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-    mysql -e "FLUSH PRIVILEGES;"
+    until mysqladmin ping -u root --silent; do
+        sleep 1
+    done
 
-    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+    mysql -u root << EOF
+    FLUSH PRIVILEGES;
+    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${MYSQL_DB}\`.* TO '${MYSQL_USER}'@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
+EOF
+
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+    wait
 fi
 
 exec mysqld_safe
